667cfa2d055596e50575c51e7ea3452a666bcb95
bongisipho325 <bongisiphomntambo@gmail.com>
2020-03-11 12:59:57 +0200

    added a few test cases

---
+++ app/src/androidTest/java/com/wits/project/money_market/activities/SellItemActivityTest.java
@@ -0,0 +1,70 @@
+package com.wits.project.money_market.activities;
+
+import android.support.design.widget.TextInputEditText;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import com.wits.project.money_market.R;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.InstrumentationRegistry.getInstrumentation;
+import static android.support.test.internal.runner.junit4.statement.UiThreadStatement.runOnUiThread;
+import static org.junit.Assert.*;
+
+@RunWith(AndroidJUnit4.class)
+public class SellItemActivityTest {
+
+    @Rule
+    public ActivityTestRule<SellItemActivity> activityTestRule = new ActivityTestRule<>(SellItemActivity.class);
+
+    @Test
+    public void sellFailToSubmitToServer(){
+        try{
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    //pressing the button without previously inputting text
+                    activityTestRule.getActivity().findViewById(R.id.sell_button).performClick();
+                }
+            });
+        } catch (Throwable throwable) {
+            throwable.printStackTrace();
+        }
+    }
+
+    @Test
+    public void sellPassToSubmitToServer(){
+        try{
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    //input text then press sell button
+                    TextInputEditText itemName = activityTestRule.getActivity().findViewById(R.id.sell_item_name),
+                            itemDesc = activityTestRule.getActivity().findViewById(R.id.sell_item_desc),
+                            itemPrice = activityTestRule.getActivity().findViewById(R.id.sell_item_price);
+
+                    itemName.setText("sddfg");
+                    itemDesc.setText("dsfdsf");
+                    itemPrice.setText("12.5");
+
+                    activityTestRule.getActivity().findViewById(R.id.sell_button).performClick();
+                }
+            });
+        } catch (Throwable throwable) {
+            throwable.printStackTrace();
+        }
+    }
+
+    /*@Test
+    public void test(){
+        getInstrumentation().runOnMainSync(new Runnable() {
+            @Override
+            public void run() {
+
+            }
+        });
+    }*/
+}
